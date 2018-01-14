d221ff6aca5fcddf2124a86e7aaf43fe29e2f2a1
k950087 <k0950087@gmail.com>
2018-01-15 05:44:09 +0800

    Add files via upload

--- FileToMysql.py
+++ FileToMysql.py
@@ -10,10 +10,10 @@ ip='140.120.54.129'
 port=3306
 folderPath='C:\CK_TEST2'    #文件夾路徑
 ############################################
-db = create_engine('mysql://'+acc+':'+str(pwd)+'@'+ip+':'+str(port)) #connect mysql
+db = create_engine('mysql://'+acc+':'+str(pwd)+'@'+ip+':'+str(port)+'/'+dbName) #connect mysql
 
-db.execute('CREATE DATABASE '+dbName+' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci') #create database
-db.execute('USE '+dbName) #use database
+#db.execute('CREATE DATABASE '+dbName+' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci') #create database
+#db.execute('USE '+dbName) #use database
 
 db.execute('CREATE TABLE '+tbName+'(DATE_TIME datetime ,PRO_TOTALACTIVEPOWER varchar(50), HOUR_TOTALWINDOK varchar(50),'
                                   'ROTOR_RPM varchar(50) ,HOUR_TOTALSERVICEON varchar(50) ,HOUR_TOTALYAW varchar(50),'
@@ -26,6 +26,7 @@ db.execute('CREATE TABLE '+tbName+'(DATE_TIME datetime ,PRO_TOTALACTIVEPOWER var
 
 
 filePath=os.listdir(folderPath)             #文件夾下有哪些檔案
+filePath=sorted(filePath,key=len)           #檔名依長度排序
 fileCount=''.join(filePath).count('.csv')   #合併字串後計算字元
 
 #讀檔後匯入mysql
@@ -34,6 +35,6 @@ for i in range(fileCount):
     read_file = pd.read_csv(file,header=None)
     data=read_file[0:][2:]
     data.columns=['DATE_TIME','PRO_TOTALACTIVEPOWER','HOUR_TOTALWINDOK','ROTOR_RPM','HOUR_TOTALSERVICEON','HOUR_TOTALYAW','HYD_OILPRESSURE','GEAR_BEARINGTEMPERATURE','GRID_FREQUENCY','GEAR_OILTEMPERATURE' ,'GRID_BUSBARTEMPERATURE' ,'HYD_OILTEMPERATURE','CON_TOPTEMPERATURE','GEN_SLIPRINGTEMPERATURE','NAC_TEMPERATURE' ,'CON_HUBTEMPERATURE' ,'AMB_TEMPERATURE' ,'SPINNER_TEMPERATURE' ,'AMB_WINDSPEED' ,'ROTO_ROTORRPM' ,'NAC_DIRECTION' ,'ROTO_BLADESPITCHANGLE']
-    data['CK_NUMBER']=i+1
+    data['CK_NUMBER'] = int(i / 3 + 1)
     data.to_sql(name=tbName, con=db, if_exists='append', index=False)   #input database
 
